ac91e1ae8974e4442475c249ac8c6fe452c485f1
DeathWalker47 <fizer_00@mail.ru>
2021-12-13 17:38:37 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,77 @@
+$(function(){
+  
+});
+
+let accardeon = document.querySelectorAll('.accardeon__item');
+
+accardeon.forEach(el => {
+  el.addEventListener('click', (e) => {
+    const self = e.currentTarget;
+    const content = self.querySelector('.accardeon__content');
+
+    // Проходимся циклом по каждому аккардеону и убираем у всех у них класс опен
+    document.querySelectorAll('.accardeon__item').forEach(el => {
+      el.classList.remove('accardeon__item--open')
+      el.querySelector('.accardeon__content').style.maxHeight = null;
+    })
+    
+    self.classList.toggle('accardeon__item--open');
+
+    if(self.classList.contains('accardeon__item--open')) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = null;
+    }
+  })
+})
+
+
+new Swiper('.reviews-swiper',{
+  loop:true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  spaceBetween: 30,
+  speed: 800,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+ });
+
+new Swiper('.news-swiper',{
+  loop:true,
+  slidesPerView: 2,
+  spaceBetween: 30,
+  speed: 800,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ });
+ const item = document.querySelectorAll('.progress__item')
+//  const line = document.querySelectorAll('.progress__line')
+ item.forEach(el => { //проходимся по блоку
+
+  let line = el.querySelector('.progress__line')
+  let percent = line.dataset.percent; //нашли атрибут блока с процентами %
+  console.log(percent);
+  let maxWidthLine = el.querySelector('.progress-max').getAttribute('width')
+
+  let lineProgress = el.querySelector('.progress-percent') //нашли прогресс линию
+  let lineWidth = lineProgress.getAttribute('width'); //атрибут длины прогресс линии
+
+  let circle = el.querySelector('circle')
+  let circlePosition = circle.getAttribute('cx')
+
+  let textPercent = el.querySelector('.progress__percent')
+
+  if(line.dataset.percent) {
+    let percentProggrass = maxWidthLine * (percent / 100);
+    lineProgress.setAttribute('width', percentProggrass)
+    circle.setAttribute('cx', percentProggrass)
+    textPercent.style.left = (percent - 2) + '%'
+    textPercent.textContent = percent + '%'
+  } 
+ })
---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss            = require('gulp-sass');
+const concat          = require('gulp-concat');
+const autoprefixer    = require('gulp-autoprefixer');
+const uglify          = require('gulp-uglify');
+const svgSprite       = require('gulp-svg-sprite');
+const imagemin        = require('gulp-imagemin');
+const fileinclude     = require('gulp-file-include');
+const del             = require('del');  
+const browserSync     = require('browser-sync').create();
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    },
+    notify: false
+  })
+}
+
+const svgSprites = () => {
+  return src('app/images/svg/**.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../sprite.svg" 
+        }
+      },
+    }))
+    .pipe(dest('app/images'));
+}
+
+function styles() {
+  return src('app/scss/style.scss')
+    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())  
+}
+
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/swiper/swiper-bundle.js',
+    'app/js/main.js'
+  ])
+  .pipe(concat('main.min.js'))
+  .pipe(uglify())
+  .pipe(dest('app/js'))
+  .pipe(browserSync.stream())   
+}
+
+
+function images() {
+  return src('app/images/**/*.*')
+  .pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.mozjpeg({quality: 75, progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({
+      plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+        ]
+    })
+  ]))
+  .pipe(dest('dist/images'))
+}
+
+function htmlInclude() {
+  return src(['app/pages/*.html'])
+  .pipe(fileinclude({
+    prefix: '@',
+    basepath: '@file'
+  }))
+  .pipe(dest('app'))
+  .pipe(browserSync.stream());
+}
+
+function build() {
+  return src([
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/js/main.min.js',
+    'app/fonts/*.*'
+  ], {base: 'app'})
+  .pipe(dest('dist'))
+}
+
+function cleanDist() {
+  return del('dist')
+}
+
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/pages/**/*.html'], htmlInclude);
+  watch('app/images/svg/**.svg', svgSprites);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+
+
+exports.styles        = styles;
+exports.scripts       = scripts;
+exports.fileinclude   = htmlInclude;
+exports.browsersync   = browsersync;
+exports.watching      = watching;
+exports.images        = images;
+exports.cleanDist     = cleanDist;
+exports.build         = series(cleanDist, images, svgSprites, build);
+
+exports.default = parallel(htmlInclude, styles, scripts, browsersync, svgSprites, watching);
